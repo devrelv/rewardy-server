054d86b084fdfaacb7027a26ac1c11b369d57f60
ItamarM <itamarmula@gmail.com>
2017-10-15 09:50:52 +0300

    add voucher base class

---
+++ bot/bot/core/voucher.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function Voucher(voucherId, title, description, imageUrl, points) {
+    this.voucherId = voucherId;
+    this.title = title;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.points = points;
+
+    Object.freeze(this);
+}
+
+module.exports = Voucher;
